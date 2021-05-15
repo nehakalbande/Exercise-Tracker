52b504402bd8d539b8ef3d63cea75f9fa03026e0
Neha Kalbande <65285162+nehakalbande@users.noreply.github.com>
2021-05-15 17:04:18 +0530

    Add files via upload

---
+++ build/precache-manifest.81507802b14f9ed8259a023e55adb697.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9228a475d3c738396d818970d54f8ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ef743c81d76c0e6c9a2",
+    "url": "/static/css/2.ce595351.chunk.css"
+  },
+  {
+    "revision": "7ef743c81d76c0e6c9a2",
+    "url": "/static/js/2.6ab02011.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.6ab02011.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7031c8dd5bde9a97aa0e",
+    "url": "/static/js/main.5e35bc6d.chunk.js"
+  },
+  {
+    "revision": "a37af620878b53014277",
+    "url": "/static/js/runtime-main.0af7428b.js"
+  }
+]);
